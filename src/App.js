0dd0a53bb7bc199cb29144ef1d4543a2495755e2
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2019-06-13 15:38:14 -0600

    first two http methods created in app.js

--- src/App.js
+++ src/App.js
@@ -22,13 +22,57 @@ class App extends Component {
     }
   }
 
+  async postMess = (data) => {
+    let response = await fetch(API, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    if (response.status === 200) {
+      const json = await response.json()
+      this.setState({
+        ...this.state.formHidden,
+        messages: [
+          ...this.state.messages,
+          json
+        ]
+      })
+    } else {
+      console.log('couldn\'t post message', response.status)
+    }
+  }
+
+  async deleteMess = (id) => {
+    const response = await fetch(`${API}, ${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: id
+    })
+    if (response.status === 200) {
+      const json = await response.json()
+      const removedId = json.id
+      this.setState({
+        ...this.state,
+        messages: [...this.state.messages.filter(message => message.id !== removedId)]
+      })
+    }
+  }
+
+  async patchMess = () => {
+    let response = await fetch(API, {})
+  }
+
   composeToggle = () => {
     this.state.formHidden === 'hidden'
       ? this.setState({formHidden: ''})
       : this.setState({formHidden: 'hidden'})
   }
-
-
 
   render() {
     console.log(this.state.messages)
